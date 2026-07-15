40a6cca56e19d923d3af1b0f6e78a292e241043c
TKtkGg <yuki.miyamoto.0729@icloud.com>
2026-07-15 11:21:04 +0900

    UsersテーブルのEntity作成

---
+++ backend/src/main/java/com/example/backend/entity/Users.java
@@ -0,0 +1,61 @@
+package com.example.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+
+@Entity
+@Table(name = "users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "username", nullable = true)
+    private String username;
+
+    @Column(name = "email", nullable = true, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = true)
+    private String password;
+
+    
+    public Integer getId() {
+        return this.id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
