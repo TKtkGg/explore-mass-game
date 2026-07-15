4fabf6316b625cd1187218e080f64d616700b00b
TKtkGg <yuki.miyamoto.0729@icloud.com>
2026-07-15 11:52:15 +0900

    UsersPrincipal作成

---
+++ backend/src/main/java/com/example/backend/service/UsersPrincipal.java
@@ -0,0 +1,51 @@
+package com.example.backend.service;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import com.example.backend.entity.Users;
+
+public class UsersPrincipal implements UserDetails {
+    private Users users;
+
+    public UsersPrincipal(Users users) {
+        this.users = users;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return users.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return users.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
