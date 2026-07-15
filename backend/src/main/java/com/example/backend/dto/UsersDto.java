6e6d07552e5fc173c4bd5348822b1daeadb07ec1
TKtkGg <yuki.miyamoto.0729@icloud.com>
2026-07-15 11:24:52 +0900

    UsersDto作成

--- backend/src/main/java/com/example/backend/dto/UsersDto.java
+++ backend/src/main/java/com/example/backend/dto/UsersDto.java
@@ -1,5 +1,29 @@
 package com.example.backend.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Email;
+
 public class UsersDto {
     
+    @NotEmpty
+    private String username;
+
+    @NotEmpty
+    @Email
+    private String email;
+
+    @NotEmpty
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
